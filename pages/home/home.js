df2974ad6da6503b9f941e66ed59c17f9eb765e5
yuanhao666 <1026344206@qq.com>
2020-03-03 08:42:42 +0800

    06_发送网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,55 @@
 // pages/home/home.js
-Page({
+import request from '../../service/network.js'
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // --------------------1.原生方式发送网络请求------------------------
+    //发送网络请求
+    //发送最简单的get请求
+    // wx.request({
+    //   url:'http://123.207.32.32:8000/recommend',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
 
-  },
+    // 2.get请求，并且携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data: {
+    //     type:'sell',
+    //     page: 1
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // 3.post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   data: {
+    //     name: 'coderwhy'
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   },
+    //   faile: function(err) {
+    //     console.log(err)
+    //   }
+    // })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 2.使用封装的request发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/recommend'
+    })
+    // .then(res => {
+    //   console.log(res)
+    // }).catch(err => {
+    //   console.log(err)
+    // })
   }
 })
